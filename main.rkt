7ff1c3e2c35be7bf8e44127e83b491f521fa20e2
David K. Storrs <david.storrs@biomantica.com>
2018-07-06 13:24:27 -0400

    main.rkt : removed tests

--- main.rkt
+++ main.rkt
@@ -131,32 +131,3 @@
              ((field.required? (field.kw field.field-contract)) ... predicate))
             (struct-id (field.wrapper-func field.id) ...))))))))
 
-
-
-
-(struct++ thing (name)         #:transparent)
-; none
-; default
-; contract
-; contract + wrapper
-; default + contract
-; default + contract + wrapper
-(struct++ ball  (owner
-                 [(maker 'adidas)]
-                 ; error: 7 8 are spliced into contract as arguments instead of or/c
-                 [color (or/c 'red 'white 'black)]
-
-                 ; works
-                 [texture  (or/c 'rough 'smooth)]
-                 [(weight 100) exact-positive-integer?]
-                 [(shear-force 20) exact-positive-integer? number->string]
-                 )
-          #:transparent)
-
-(thing   'wilson)
-(thing++ #:name 'volleyball-friend)
-(ball++ #:owner 'bob
-        #:color 'red
-        #:texture 'rough
-        #:weight 200
-        #:shear-force 19)
