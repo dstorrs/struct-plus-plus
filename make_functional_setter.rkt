8f493c41137eb458be8decce5eead15179d98f6e
David K. Storrs <david.storrs@biomantica.com>
2018-09-28 14:53:01 -0400

    now creates functional updaters as well as functional setters

--- make_functional_setter.rkt
+++ make_functional_setter.rkt
@@ -2,7 +2,7 @@
 
 (require (for-syntax racket/syntax syntax/parse))
 
-(provide make-functional-setter)
+(provide make-functional-setter make-functional-updater)
 
 ;;     make-functional-setter: macro for generating non-mutating field
 ;;     setter functions for a struct
@@ -53,3 +53,34 @@
                #'(definer func-header
                    func-contract
                    (struct-copy type-name the-struct [field-name (wrapper val)]))]))]))]))
+
+;;----------------------------------------------------------------------
+
+(define-syntax (make-functional-updater stx)
+  (syntax-parse stx
+    #:literals (make-functional-updater)
+    
+    ; First, grab the name of the struct and the field we're making
+    ; this for.  We'll build some stuff here then re-parse instead of
+    ; copy/pasting for every pattern match
+;    (make-function-updater thing name symbol-string? symbol-string->string)
+    
+    [(make-functional-updater type-name field-name ignored ...)
+     (with-syntax* ([func-name   (format-id #'type-name "update-~a-~a" #'type-name #'field-name)]
+                    [getter      (format-id #'type-name "~a-~a" #'type-name #'field-name)]
+                    [func-header #'(func-name the-struct proc)]
+                    [definer     #'define]
+                    [type-pred   (format-id #'type-name "~a?" #'type-name)]
+                    [func-body   #'(struct-copy type-name the-struct [field-name (proc (getter the-struct))])]
+                    )
+       (syntax-parse stx
+         [(make-functional-updater type-name field-name) #'(definer func-header func-body)]
+         [(make-functional-updater type-name field-name field-contract:expr ignored ...)
+          (with-syntax ([definer #'define/contract]
+                        [func-contract #'(-> type-pred (-> any/c field-contract) type-pred)])
+            (syntax-parse stx
+              [(_ _ _ _) #'(definer func-header func-contract func-body)]
+              [(_ _ _ _ wrapper:expr)
+               #'(definer func-header
+                   func-contract
+                   (struct-copy type-name the-struct [field-name (wrapper (proc (getter the-struct)))]))]))]))]))
