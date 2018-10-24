8e54bfab7fbdd9c2a03943820bbbce1c5ef8fe88
David K. Storrs <david.storrs@biomantica.com>
2018-10-23 21:30:16 -0400

    info.rkt : added deps, scribblings, test-omit-paths

--- info.rkt
+++ info.rkt
@@ -1,4 +1,9 @@
 #lang info
+
 (define collection "struct-plus-plus")
 (define version "1.0")
+(define deps '("syntax-classes-lib"))
+(define scribblings '(("scribblings/struct-plus-plus.scrbl")))
 
+(define test-omit-paths '("test_main.rkt"
+                          "test_make_functional_setter.rkt"))
