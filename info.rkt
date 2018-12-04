fdd8170670a3eef4f9e01c34313206487954b4af
David K. Storrs <david.storrs@biomantica.com>
2018-12-04 17:18:09 -0500

    info.rkt: added handy so that the pkg server wouldn't choke when it hits the tests

--- info.rkt
+++ info.rkt
@@ -2,7 +2,8 @@
 
 (define collection "struct-plus-plus")
 (define version "1.0")
-(define deps '("syntax-classes-lib"))
+(define deps '("handy"
+               "syntax-classes-lib"))
 (define scribblings '(("scribblings/struct-plus-plus.scrbl")))
 
 (define test-omit-paths '("test_main.rkt"
