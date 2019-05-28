5c06b6141cd792ef044e55b832aa310ae56dc2c4
David K. Storrs <david.storrs@biomantica.com>
2019-05-28 15:01:48 -0400

    bumped to version 4 because the #:omit-reflection keyword is a major new feature

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "struct-plus-plus")
-(define version "3.8")
+(define version "4.0")
 (define deps '("base"
                "handy"
                "syntax-classes-lib"))
