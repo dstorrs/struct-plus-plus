4d05a611ff0f490b029093914dadaceb5e91877a
David K. Storrs <david.storrs@biomantica.com>
2019-07-26 12:54:00 -0400

    info.rkt: bumped version num, added sandbox-lib to build deps

--- info.rkt
+++ info.rkt
@@ -1,14 +1,16 @@
 #lang info
 
 (define collection "struct-plus-plus")
-(define version "4.1")
+(define version "5.0")
 (define deps '("base"
                "handy"
-               "syntax-classes-lib"))
+               "syntax-classes-lib"
+               ))
 (define scribblings '(("scribblings/struct-plus-plus.scrbl" ())))
 
 (define test-omit-paths '("test_main.rkt"
                           "test_make_functional_setter.rkt"))
 (define build-deps '("at-exp-lib"
                      "racket-doc"
-                     "scribble-lib"))
+                     "scribble-lib"
+                     "sandbox-lib"))
